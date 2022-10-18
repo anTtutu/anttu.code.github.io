1f946163d4d357b70611634a368e8f3a9b226e8c
Anttu <anttu0531@gmail.com>
2022-10-18 20:56:23 +0800

    add ksuid

---
+++ src/test/java/com/anttu/demo/TestKSUID.java
@@ -0,0 +1,19 @@
+package com.anttu.demo;
+
+import org.junit.Test;
+
+/**
+ * 描述
+ *
+ * @ClassName：TestKSUID
+ * @Description：测试 KSUID
+ * @author：Anttu
+ * @Date：18/10/2022 16:28
+ */
+public class TestKSUID {
+
+    @Test
+    public void generateKSUID () {
+
+    }
+}
